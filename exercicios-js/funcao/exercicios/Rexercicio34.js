acb61c4aa048d5706eddca6bbbf770caed12562d
arthurprioli <arthur.prioli@gmail.com>
2020-04-19 20:51:49 -0300

    Update Rexercicio34.js

--- exercicios-js/funcao/exercicios/Rexercicio34.js
+++ exercicios-js/funcao/exercicios/Rexercicio34.js
@@ -1,21 +1,6 @@
-function compararString(string1, string2){
+    let string1 = 'arthur'
+    let string2 = 'ARTHURITO'
     let stringLower1 = string1.toLowerCase()
     let stringLower2 = string2.toLowerCase()
 
-    let iguais = []
-    for (char in stringLower1){
-        for (c in stringLower2){
-            if (stringLower2[c] == stringLower1[c]){
-                iguais.push(stringLower2[c])
-            }
-        }
-    }
-    console.log(iguais)
-    if (iguais == string1 || iguais == string2){
-        return true
-    } else {
-        return false
-    }
-}
-
-console.log(compararString('arthur', 'ARTHUR'))
+    console.log(stringLower1.includes(stringLower2), stringLower2.includes(stringLower1))
