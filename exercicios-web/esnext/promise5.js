de85dd3fcb075b18ffe1ecf6a190ea7dd3d005ed
Arthur Prioli <arthur.prioli@gmail.com>
2021-01-24 14:30:07 -0300

    acabei o capitudo de ecma

---
+++ exercicios-web/esnext/promise5.js
@@ -0,0 +1,20 @@
+function funcionarOuNao(valor, chanceErro){
+    return new Promise((resolve, reject) => {
+        try {
+            con.log(temp)
+            if (Math.random() < chanceErro) {
+                reject('Ocorreu um erro!')
+            } else {
+                resolve(valor)
+            }
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+
+funcionarOuNao('Testando...', 0.9)
+    .then(v => console.log(`Valor ${v}`))
+    .then(v => consol.log(v), err => console.log(`Erro específico: ${err}`))
+    .catch(err => console.log(`Erro: ${err}`))
+    .then(console.log('Fim!'))
